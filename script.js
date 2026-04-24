ca131730f89a8d15334b9c9d15c313f5a7e07443
Jackson tech <jacksonlumumba275@gmail.com>
2026-04-24 21:47:38 +0300

    Update orderNow alert and add orderWhatsApp function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function orderNow() {
-    alert("Call 0741590397 to place your order!");
+    alert("Call 0741590398 to place your order!");
+}
+
+function orderWhatsApp() {
+    window.open("https://wa.me/254741590397");
 }
